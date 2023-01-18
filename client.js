3f224e3be765edfe016f0de0df080a0cdfb172d1
Graham Fujisawa <sfrrki@gmail.com>
2023-01-18 13:55:04 -0800

    using set intervalto continuously go up and left on connect

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 const net = require("net");
+const connections = [];
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243', // IP address here,
     port: 50541, // PORT number here,
@@ -10,15 +11,38 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", (data) => {
+  conn.on("connect", () => {
     // code that does something when the connection is first established
-    console.log('Connected! we should get undefined', data)
+    conn.write('Name: Jim');
+    // conn.write("Move: up");
+    setInterval(() => {
+      conn.write("Move: left");
+      conn.write("Move: up");
+    }, 500);
+    conn.write("Move: left");
+    conn.write("Move: up");
+    conn.write("Move: left");
+    conn.write("Move: up");
+
   });
   conn.on("data", (data) => {
     // code that does something when the connection is first established
-    console.log('server data:', data)
+    console.log('server data:', data);
   });
 
+
+
+
+  // if (data.includes('Name:')) {
+  //   console.log('this client will change their name');
+
+  // } else {
+  //   for (const connection of connections){
+  //     if (connection !== conn) {
+  //       connection.write(data);
+  //     }
+  //   }
+  // }
   return conn;
 };
 
